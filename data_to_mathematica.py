8a0e2a963f6787f3ce84e59a0e12c10c70e490d4
Derek Harnett <derek.harnett@shaw.ca>
2024-06-01 13:57:54 -0700

    initial commit

---
+++ data_to_mathematica.py
@@ -0,0 +1,11 @@
+"""
+Convert pySecDec data to a Mathematica-friendly format.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ make_data.py
@@ -0,0 +1,11 @@
+"""
+Generate pySecDec data.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
