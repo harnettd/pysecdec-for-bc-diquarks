10f5aa53d496f2b345bf41c9ee6ed477dcd77cf3
Derek Harnett <derek.harnett@shaw.ca>
2024-06-20 14:41:35 -0700

    implement TarcerBasisIntegral class

---
+++ src/tarcer_basis_integral.py
@@ -0,0 +1,45 @@
+"""Tarcer basis integral class"""
+from pySecDec.integral_interface import DistevalLibrary
+
+
+class TarcerBasisIntegral:
+
+    def __init__(
+            self, 
+            specification_path: str, 
+            squared_masses: dict[str],
+            workers=None,
+            verbose=False
+        ) -> None:
+        """
+        Define a Tarcer basis integral
+        """
+        self.disteval_lib =\
+            DistevalLibrary(specification_path, workers, verbose)
+        self.squared_masses = squared_masses
+        
+    def eval(self, qq: str, **kwargs) -> str:
+        """
+        Evaluate an integral.
+        """
+        parameters = {**self.squared_masses}
+        parameters['qq'] = qq
+        result = self.disteval_lib(parameters=parameters, **kwargs)
+        return result
+
+
+if __name__ == '__main__':
+    # print(__doc__)
+
+    from pathlib import Path
+    
+    project_path = Path(__file__).parent.parent
+    lib_path = project_path.joinpath('lib')
+    json_path = lib_path\
+        .joinpath('TBI_1_m1_1_m2')\
+        .joinpath('disteval')\
+        .joinpath('TBI_1_m1_1_m2.json')
+    
+    tbi = TarcerBasisIntegral(json_path, {'m1m1': '1.2', 'm2m2': '4.2'})
+    result = tbi.eval(qq='10.0', format='mathematica', epsrel=1e-6, epsabs=1e-9)
+    print(result)
