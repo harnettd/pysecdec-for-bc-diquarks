a2478f627e8d2b48a2bb7d7015de4c360cab2d05
Derek Harnett <derek.harnett@shaw.ca>
2024-07-22 11:52:42 -0700

    update additional_prefactor calculation

--- pysecdec_integrals/get_additional_prefactor/get_additional_prefactor.py
+++ pysecdec_integrals/get_additional_prefactor/get_additional_prefactor.py
@@ -5,7 +5,7 @@ from sympy import symbols, sympify
 def get_additional_prefactor(
         dimensionality, 
         num_loops: int, 
-        masses: list[float, float]
+        renorm_scale: float
     ):
     """
     Return the additional_prefactor parameter for a pySecDec generate file.
@@ -16,8 +16,8 @@ def get_additional_prefactor(
     :param num_loops: The number of loops in the integral
     :type num_loops: int
 
-    :param masses: Two propagator masses
-    :type masses: list[float, float]
+    :param renorm_scale: Renormalization scale
+    :type renorm_scale: float
 
     :return: The additional_prefactor parameter value
     :rtype: A sympy expression
@@ -25,16 +25,15 @@ def get_additional_prefactor(
     Usage examples:
     >>> from sympy import simplify
     >>> eps = symbols('eps')
-    >>> calc = get_additional_prefactor(4 + 2 * eps, 1, [1, 3])
-    >>> actual = sympify("-I / ((pi ** 2) * ((4 * pi) ** eps))")
+    >>> calc = get_additional_prefactor(4 + 2 * eps, 1, 2.0)
+    >>> actual = sympify("I / (4.0 ** eps)")
     >>> simplify(calc - actual)
     0
     """
-    scale = (masses[0] + masses[1]) / 2  # renormalization scale
     dim = symbols('dim')  # spacetime dimension
 
     additional_prefactor_per_loop =\
-        sympify(f'{scale} ** (4 - dim) / (I * pi ** (dim / 2))')
+        sympify(f'I * {renorm_scale} ** (4 - dim)')
 
     additional_prefactor = additional_prefactor_per_loop ** num_loops
 
