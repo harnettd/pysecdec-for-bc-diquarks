a573683ab0af282c1763ccbf9d13b1fee22fbc7e
Derek Harnett <DHarnett.dev@proton.me>
2025-01-28 15:30:41 -0800

    add test_integrate.py skeleton

---
+++ test/test_integrate.py
@@ -0,0 +1,9 @@
+"""Test integrate."""
+import unittest
+
+class TestIntegrate(unittest.TestCase):
+    def test_integrate(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
